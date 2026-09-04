22ec3bedb0bc4d160863636d59204765602ef9b7
Ranjeet Kurmi <ranjeet.cs231167@global.org.in>
2026-09-04 16:43:59 +0530

    varriable in javascript

---
+++ 1.js
@@ -0,0 +1,11 @@
+//const m value change nhi hoti fix value rhti h
+const country = "India";
+const birthyear = 2000;
+
+let age = 25;
+ console.log(country);
+ console.log(birthyear);
+
+ age = 26;
+
+ console.log(age);
