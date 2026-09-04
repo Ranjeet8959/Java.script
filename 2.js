21bc4c457cfbe5d2751f568129a4a89ec662502d
Ranjeet Kurmi <ranjeet.cs231167@global.org.in>
2026-09-04 16:51:20 +0530

    varriable

---
+++ 2.js
@@ -0,0 +1,16 @@
+//let m varriable ki value change kr skte h
+let age = 28;
+age = 45;
+
+
+//const ki value change nhi hoti h 
+
+const Name = "Ranjeet";
+const country = "India";
+
+const village = "Sankui";
+
+console.log(age);
+console.log(Name);
+console.log(country);
+console.log(village);
